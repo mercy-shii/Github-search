dc83786b0171ccce3810abd185941e8a81b68a8a
mercy-shii <wemercy525@gmail.com>
2020-04-09 14:19:08 +0300

    Github-search

--- src/app/service/profile.service.ts
+++ src/app/service/profile.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Http,Headers } from '@angular/http';
+import 'rxjs/add/Operator/map';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  private username: 'string';
+  private clientid = '60660187';
+  private clientsecret ='dac068760c34b41ba8348d630f772fe10947f8c7';
+
+  constructor(private http:Http) { }
 }
