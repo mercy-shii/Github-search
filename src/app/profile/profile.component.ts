c565b0d88d3094eefe411280b4d42ffac19f19c0
mercy-shii <wemercy525@gmail.com>
2020-04-09 16:25:10 +0300

    Github-search

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -14,6 +14,7 @@ export class ProfileComponent implements OnInit {
 
     this.profileservice.getProfileInfo().subscribe(profile => {
       console.log(profile);
+      this.profile = profile;
     });
   }
 
