c7366a80ffa6029d29aa46a21271d44b8ad0d71f
mercy-shii <wemercy525@gmail.com>
2020-04-11 12:58:32 +0300

    Github-search

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../service/profile.service';
 import { profile } from '../profile';
+import { RepoListService } from '../repo-list/repo-list.service';
+import { repository } from './repository';
 
 @Component({
   selector: 'app-profile',
@@ -10,24 +12,23 @@ import { profile } from '../profile';
 export class ProfileComponent implements OnInit {
 
   myprofile: profile;
-  repos: any[];
+  repos: repository[];
   username: string;
 
-  constructor(private profileservice:ProfileService) { 
+  constructor(private profileservice:ProfileService,private repolistservice:RepoListService) { 
 
   }
 
   findProfile(user){
     //this.profileservice.updateProfile(this.username);
-    this.profileservice.getProfileInfo(user).subscribe(profile => {
-      console.log(profile);
+    this.profileservice.getProfileInfo(user).subscribe(profile =>{
       this.myprofile = profile;
     });
 
-
-    this.profileservice.getprofileRepos().subscribe(repos => {
-      console.log(this.repos);
-      this.repos = this.repos;
+  
+    this.repolistservice.getRepos(user).subscribe(repository => {
+  
+      this.repos = repository;
     });
   }
 
